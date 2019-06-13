f903386fbb0753bdc4286d5bd25347f40e8661fc
jxk165130 <40884275+jxk165130@users.noreply.github.com>
2019-06-13 01:15:37 -0600

    Update Myclass.java

--- Myclass.java
+++ Myclass.java
@@ -9,6 +9,23 @@ public class Myclass {
        car ca = new car();
        person per = new person();
 
+       // check email String for UI application
+       String str = "jingmailcom";
+       boolean haha = str.contains("@");
+       boolean hoho = str.contains(".");
+       if(hoho && haha){
+           System.out.println("email contains the both dot and @");
+       }
+       else if (!haha) {
+           System.out.println("email does not contains the @");
+       }
+       else if(!hoho){
+           System.out.println("email does not contain .");
+       }
+       else
+           System.out.println("email does not contain both @ and .");
+
+        // practice inheritance
         per.setYear(1990);
         per.setMake("honda");
         System.out.println(per);
